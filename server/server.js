4d0499878bc20d39ed83975f561f6a68fde49502
Kerry <trankerry94@gmail.com>
2021-11-23 15:22:18 -0800

    updated scripts

--- server/server.js
+++ server/server.js
@@ -1,11 +1,7 @@
 const express = require('express');
-const morgan = require('morgan');
 
 const app = express();
 
-app.use(morgan('dev'));
 app.use(express.static('build'));
-app.use(express.static('dist'));
-app.use(express.static('public'));
 
 module.exports = app;
